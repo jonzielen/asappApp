f7f91f7c0ece1dbcd46d25704226f0d1507352f7
jonzielen <jonzielen@gmail.com>
2021-07-03 19:09:37 -0400

    add save feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,9 @@ export class AppComponent implements OnInit {
   title: string = 'asappApp';
 	showErrorMessage: boolean = false;
 	errorMessage: string = 'There was an error, please try again later.'
-	citiesData: Array<CityInfo>;
-	locationSearchText: string;
+	fullSearchData: Array<CityInfo>;
+	filteredSearchData: Array<CityInfo>;
+	savedLocations: Array<CityInfo> = [];
 
   constructor(private http: HttpClient) { }
 
@@ -31,18 +32,60 @@ export class AppComponent implements OnInit {
 
 		this.http.get(apiUrl)
     .subscribe(cities => {
-			console.log(cities);
 
-      // this.citiesData = cities['data'];
-			this.citiesData = cities['data'].slice(0, 199); // work with manageable subset for now
+      this.fullSearchData = cities['data'].slice(0,200); // remove slice limit
+			this.filteredSearchData = this.fullSearchData;
     },
     error => {
 			this.showErrorMessage = true;
     });
   }
 
-	getSearchText(e) {
-    this.locationSearchText = e;
+	// change name
+	updateSearchText(e: string) {
+		this.filteredSearchData = this.filterData(e);
   }
+
+	filterData(searchText: string) {
+		// default to show all search data
+		if (!this.filteredSearchData || !searchText) return this.fullSearchData;
+
+		return this.fullSearchData.filter(function(location: CityInfo) {
+			for (let prop in location) {
+				// search
+				if (location[prop].toString().toLowerCase().search(new RegExp(searchText, 'i')) > -1) return location;
+			}
+		});
+	}
+
+	addToSaved(event: Event, locationData: CityInfo, index: number) {
+		// add data to saved array
+		this.savedLocations.push(locationData);
+
+		// remove item from master array
+		this.fullSearchData.splice(index, 1);
+
+		// remove data from filtered array
+		this.filteredSearchData = this.fullSearchData;
+	}
+
+	removeFromSaved(event: Event, locationData: CityInfo, index: number) {
+		// add data to saved array
+		this.savedLocations.splice(index, 1);
+
+		// remove item from master array
+		this.fullSearchData.push(locationData);
+
+		// remove data from filtered array
+		this.filteredSearchData = this.fullSearchData;
+	}
+
+
+
+
+
+
+
+
 
 }

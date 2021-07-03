cd74fa5f4bf97b009cf43e8bb22007731ffdb456
jonzielen <jonzielen@gmail.com>
2021-07-03 12:33:57 -0400

    adds api call and basic display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'asappApp';
+export class AppComponent implements OnInit {
+  title: string = 'asappApp';
+	showErrorMessage: boolean = false;
+	errorMessage: string = 'There was an error, please try again later.'
+	citiesData: Array<{ [key: string]: string | number }>;
+
+  constructor(private http: HttpClient) { }
+
+	ngOnInit(): void {
+    this.fetchData();
+  }
+
+	private fetchData() {
+		const apiUrl = 'http://localhost:3030/cities';
+
+		this.http.get(apiUrl)
+    .subscribe(cities => {
+			console.log(cities);
+
+      // this.citiesData = cities['data'];
+			this.citiesData = cities['data'].slice(0, 199); // work with manageable subset for now
+    },
+    error => {
+			this.showErrorMessage = true;
+    });
+  }
+
 }
